638515f7cdcb69f0e47dada276b750fcbb2e43c4
Ivan Perlov <56824839+Torusaynim@users.noreply.github.com>
2021-03-22 12:50:46 +0300

    Some fixes

--- src/main/java/ru/mirea/trpp_second_8/controllers/BookController.java
+++ src/main/java/ru/mirea/trpp_second_8/controllers/BookController.java
@@ -4,6 +4,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import ru.mirea.trpp_second_8.entity.Book;
 
 import java.io.InputStreamReader;
 import java.util.List;
